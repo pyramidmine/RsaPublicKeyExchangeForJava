e9024ba41512bd1a135df38b0ec5de0fb8edd765
pyramidmine <pyramidmine@gmail.com>
2020-02-14 15:20:38 +0900

    Signing/Verification 추가

--- src/rsa/RsaTest.java
+++ src/rsa/RsaTest.java
@@ -16,10 +16,13 @@ import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.SecureRandom;
+import java.security.Signature;
+import java.security.SignatureException;
 import java.security.interfaces.RSAPublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
+import java.util.Arrays;
 import java.util.Base64;
 
 import javax.crypto.BadPaddingException;
@@ -32,7 +35,7 @@ public class RsaTest {
 	static final String SAMPLE_TEXT = "O2OSYS";
 	static final byte[] SAMPLE_DATA = SAMPLE_TEXT.getBytes(StandardCharsets.UTF_8);
 	static final int RSA_KEY_BIT_SIZE = 2048;
-	
+	static final String SIGNATURE_ALGORITHM = "SHA1withRSA";
 	static final int FILE_BUFFER_SIZE = 2048;
 	
 	public static void main(String[] args) {
@@ -71,17 +74,29 @@ public class RsaTest {
 		byte[] decryptedData = decrypt(encryptedData, privateKeyData);
 		
 		// 결과
-		System.out.println("---------- RSA, Key: Java, Endec: Java ----------");
+		System.out.println("---------- RSA, Key: Java, Apply: Java ----------");
 		System.out.println("Original Data : " + Base64.getEncoder().encodeToString(SAMPLE_DATA));
 		System.out.println("Decrypted Data: " + Base64.getEncoder().encodeToString(decryptedData));
+
+		// Signing/Verification
+		System.out.println("---------- RSA Signing/Verification, Key: Java, Apply: Java ----------");
+		byte[] signature = sign(SAMPLE_DATA, SIGNATURE_ALGORITHM, privateKeyData);
+		
+		// Verification
+		boolean verified = verify(SAMPLE_DATA, SIGNATURE_ALGORITHM, signature, publicKeyData);
+		System.out.println("Original Data Verification: " + verified);
+		
+		// 조작된 데이터로 검증
+		byte[] fakeSampleData = Arrays.copyOf(SAMPLE_DATA, SAMPLE_DATA.length);
+		fakeSampleData[5] += 1;
+		verified = verify(fakeSampleData, SIGNATURE_ALGORITHM, signature, publicKeyData);
+		System.out.println("Fake Data Verification: " + verified);
 	}
 	
 	private static byte[] encrypt(byte[] data, byte[] publicKeyData) {
 		byte[] encryptedData = null;
 		try {
-			X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(publicKeyData);
-			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-			PublicKey publicKey = keyFactory.generatePublic(publicKeySpec);
+			PublicKey publicKey = createPublicKey(publicKeyData);
 			Cipher encryptor = Cipher.getInstance("RSA");
 			encryptor.init(Cipher.ENCRYPT_MODE, publicKey);
 			encryptedData = encryptor.doFinal(data);
@@ -95,8 +110,6 @@ public class RsaTest {
 			e.printStackTrace();
 		} catch (BadPaddingException e) {
 			e.printStackTrace();
-		} catch (InvalidKeySpecException e) {
-			e.printStackTrace();
 		}
 		return encryptedData;
 	}
@@ -104,9 +117,7 @@ public class RsaTest {
 	private static byte[] decrypt(byte[] data, byte[] privateKeyData) {
 		byte[] decryptedData = null;
 		try {
-			PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(privateKeyData);
-			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-			PrivateKey privateKey = keyFactory.generatePrivate(privateKeySpec);
+			PrivateKey privateKey = createPrivateKey(privateKeyData);
 			Cipher decryptor = Cipher.getInstance("RSA");
 			decryptor.init(Cipher.DECRYPT_MODE, privateKey);
 			decryptedData = decryptor.doFinal(data);
@@ -120,10 +131,60 @@ public class RsaTest {
 			e.printStackTrace();
 		} catch (BadPaddingException e) {
 			e.printStackTrace();
-		} catch (InvalidKeySpecException e) {
-			e.printStackTrace();
 		}
 		return decryptedData;
+	}
+	
+	private static byte[] sign(byte[] data, String algorithm, byte[] privateKeyData) {
+		byte[] result = null;
+		try {
+			PrivateKey privateKey = createPrivateKey(privateKeyData);
+			Signature signature = Signature.getInstance(algorithm);
+			signature.initSign(privateKey);
+			signature.update(data);
+			result = signature.sign();
+		} catch (NoSuchAlgorithmException | InvalidKeyException | SignatureException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	private static boolean verify(byte[] data, String algorithm, byte[] signedData, byte[] publicKeyData) {
+		boolean result = false;
+		try {
+			PublicKey publicKey = createPublicKey(publicKeyData);
+			Signature signature = Signature.getInstance(algorithm);
+			signature.initVerify(publicKey);
+			signature.update(data);
+			result = signature.verify(signedData);
+		} catch (NoSuchAlgorithmException | InvalidKeyException | SignatureException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	private static PrivateKey createPrivateKey(byte[] privateKeyData) {
+		PrivateKey privateKey = null;
+		try {
+			PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(privateKeyData);
+			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+			privateKey = keyFactory.generatePrivate(privateKeySpec);
+		} catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+			e.printStackTrace();
+		}
+		return privateKey;
+	}
+	
+	private static PublicKey createPublicKey(byte[] publicKeyData) {
+		PublicKey publicKey = null;
+		try {
+			X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(publicKeyData);
+			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+			publicKey = keyFactory.generatePublic(publicKeySpec);
+		} catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+			e.printStackTrace();
+		}
+		return publicKey;
 	}
 	
 	private static String getKeyDirectory() {
